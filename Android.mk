26d3bdac94f5c1ad8ea5adad025706daa852a6d2
devdaw6 <devdaw6@gmail.com>
2026-02-13 13:07:48 +0500

    Initial commit with LFS

---
+++ Android.mk
@@ -0,0 +1,31 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),cerro)
+
+$(call add-radio-file-sha1-checked,radio/aop.img,68fe45aa284211466f1e3b2a27d843a0dfa133c9)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,fbd241c4960c1ee0c8f071c86593224bfc594f30)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,956d0fb300533f653e6d25372db03e3ece58e7b7)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,535a85470db54c499fc3db706796237b5085fdf0)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,6d774318d99cf117e2a20aecc6ed06d19e99a8d7)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,b985e2f759deb11730d91c3cb5b077c3ad4b4f3d)
+$(call add-radio-file-sha1-checked,radio/dsp.img,6ce3656a7511f4fce4e44c53825ec872c2fb5f46)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,979c607ab2ecca496f19129fbd49065de45c9aba)
+$(call add-radio-file-sha1-checked,radio/hyp.img,d67cc721b0727532ccd790bd29dc092e1c028b13)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,f13d605cb9f949a2f34aa7bd1c29154430b8bd10)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,7bc5c48467acc139ee7a695a64cbad8b64bec84b)
+$(call add-radio-file-sha1-checked,radio/modem.img,b2970d955be1b70f07f7acff00f19c5bdd0a920c)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,7f18412ae615a5c2c7503cf04641d289d9947ab8)
+$(call add-radio-file-sha1-checked,radio/shrm.img,ed4185d17e27f8150c1ad2c6bf231238331c64de)
+$(call add-radio-file-sha1-checked,radio/spuservice.img,ecc24cd498bdb73f674e1c8658774d9b7ea4cbde)
+$(call add-radio-file-sha1-checked,radio/tz.img,e52c2abb8dda97bc5ed960efb3e412ac03ad1438)
+$(call add-radio-file-sha1-checked,radio/uefi.img,688e0399d790654440247d08ab71e13168dba511)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,91454a812c31a2410b565147ec1ce2610f429355)
+$(call add-radio-file-sha1-checked,radio/xbl.img,f4ee921cee690de315046a456093bbe93ae663d3)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,30fe837b4906d5b8416c24c295941ca78e966c26)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,58f6af347d847af7ab51bd3aa16406dfba4ed553)
+
+endif
